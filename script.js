b2b3069b53bb1bdd3eb1883f2d1139527a27a426
Marcos Gomes <markimmarques@hotmail.com>
2022-10-14 16:30:06 -0300

    Entrega Requisito 3

---
+++ script.js
@@ -0,0 +1,15 @@
+function loginValidation() {
+    const email = document.querySelector('#email');
+    const password = document.querySelector('#password');
+    if ( email.value === 'tryber@teste.com' && password.value === '123456') {
+        alert('Olá, Tryber!');
+      } else {
+        alert('Email ou senha inválidos.');
+      }
+    }
+    
+    window.onload = function loadedPage() {
+      const btnLogin = document.querySelector('#login');
+      btnLogin.addEventListener('click', loginValidation);
+    };
+
