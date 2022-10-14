1a466742cecbceda7f7877f68f7361023f548b10
Marcos Gomes <markimmarques@hotmail.com>
2022-10-14 16:44:54 -0300

    Entrega Requisito 4

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 function loginValidation() {
-    const email = document.querySelector('#email');
-    const password = document.querySelector('#password');
-    if ( email.value === 'tryber@teste.com' && password.value === '123456') {
-        alert('Olá, Tryber!');
-      } else {
-        alert('Email ou senha inválidos.');
-      }
-    }
-    
-    window.onload = function loadedPage() {
-      const btnLogin = document.querySelector('#login');
-      btnLogin.addEventListener('click', loginValidation);
-    };
+  const email = document.querySelector('#email');
+  const password = document.querySelector('#password');
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
 
+window.onload = function loadedPage() {
+  const btnLogin = document.querySelector('#login');
+  btnLogin.addEventListener('click', loginValidation);
+};
