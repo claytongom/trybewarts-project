900664260c43b6fd81301cb99c04d0fa6074db1a
Marcos Gomes <markimmarques@hotmail.com>
2022-10-17 20:13:16 -0300

    Requisito 20 - Bônus

--- script.js
+++ script.js
@@ -24,13 +24,13 @@ agree.addEventListener('click', () => {
   }
 });
 
-function limite_textarea(valor) {
-  quant = 500;
-  total = valor.length;
-  if(total <= quant) {
-      resto = quant - total;
-      document.getElementById('counter').innerHTML = resto;
+function limiteTextArea(valor) {
+  const quant = 500;
+  var total = valor.length;
+  if (total <= quant) {
+    resto = quant - total;
+    document.getElementById('counter').innerHTML = resto;
   } else {
-      document.getElementById('texto').value = valor.substr(0,quant);
+    document.getElementById('texto').value = valor.substr(0, quant);
   }
 }
